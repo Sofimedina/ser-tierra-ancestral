4220a99b701cae1b14c0fde3f5d17b574d458375
Sofia Medina <sofi.k.medina@gmail.com>
2024-10-30 14:17:59 -0300

    first commit
    Signed-off-by: Sofia Medina <sofi.k.medina@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { static as ghPages } from 'vite-plugin-static';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   base: '/ser-tierra-ancestral/',
 })
